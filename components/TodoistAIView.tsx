9ba7b8f6e93bc8b977bcc478a9b2ecd38c4b2b05
khirsz00-hue <khirsz00@gmail.com>
2025-10-09 21:49:42 +0200

    Update TodoistAIView.tsx

--- components/TodoistAIView.tsx
+++ components/TodoistAIView.tsx
@@ -1,19 +1,15 @@
 'use client'
 
 import { useState } from 'react'
-import ChatSidebar from './ChatSidebar'
 import GlobalChat from './GlobalChat'
 
 export default function TodoistAIView({ token }: { token: string }) {
   const [tasks, setTasks] = useState<any[]>([])
 
   return (
-    <div className="flex h-full">
-      {/* 🧠 Historia rozmów globalnych */}
-      <ChatSidebar onSelectChat={() => {}} />
-
-      {/* 🤖 Czat AI */}
-      <div className="flex-1 bg-white">
+    <div className="flex h-full bg-white rounded-b-xl border-t border-gray-200">
+      {/* 🤖 Czat AI (bez historii) */}
+      <div className="flex-1">
         <GlobalChat token={token} tasks={tasks} onOpenTaskChat={() => {}} />
       </div>
     </div>
