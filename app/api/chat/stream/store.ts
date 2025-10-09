7baf8086e5438b442081fee7ddb76ae5d09d0b48
khirsz00-hue <khirsz00@gmail.com>
2025-10-09 19:47:18 +0200

    Create store.ts

---
+++ app/api/chat/stream/store.ts
@@ -0,0 +1,15 @@
+let clients: any[] = []
+
+export function addClient(client: any) {
+  clients.push(client)
+}
+
+export function removeClient(id: number) {
+  clients = clients.filter((c) => c.id !== id)
+}
+
+export function broadcastMessage(message: any) {
+  for (const client of clients) {
+    client.send(message)
+  }
+}
